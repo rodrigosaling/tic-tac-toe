2302218917aff2749b1bb3ee4129fdea317fdd55
Rodrigo Saling <rodrigo.saling@gmail.com>
2024-11-17 19:20:36 -0300

    remove everything to start from scratch

--- packages/web/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    // include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    globals: true,
-    environment: 'happy-dom',
-  },
-});
