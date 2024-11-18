2d312a2692e866c62e8357265f5f68a52ad367aa
Rodrigo Saling <rodrigo.saling@gmail.com>
2024-11-17 23:09:54 -0300

    deno websocket logic from their docs

--- packages/server/main.ts
+++ packages/server/main.ts
@@ -1,8 +1,16 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+Deno.serve((req) => {
+  if (req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 501 });
+  }
+  
+  const { socket, response } = Deno.upgradeWebSocket(req);
+  socket.addEventListener("open", () => {
+    console.log("a client connected!");
+  });
+  socket.addEventListener("message", (event) => {
+    if (event.data === "ping") {
+      socket.send("pong");
+    }
+  });
+  return response;
+});
