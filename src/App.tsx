b4e553419f55e3722f1aa59b17be31b42f35d8df
Rodrigo Saling <rodrigosaling@gmail.com>
2025-08-31 17:33:26 -0300

    * clean up original index.css * prettier reformat all files * change index.html title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { APITester } from "./APITester";
-import "./index.css";
+import { Card, CardContent } from '@/components/ui/card';
+import { APITester } from './APITester';
+import './index.css';
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import logo from './logo.svg';
+import reactLogo from './react.svg';
 
 export function App() {
   return (
@@ -25,9 +25,11 @@ export function App() {
         <CardContent className="pt-6">
           <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
           <p>
-            Edit{" "}
-            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">src/App.tsx</code> and
-            save to test HMR
+            Edit{' '}
+            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm">
+              src/App.tsx
+            </code>{' '}
+            and save to test HMR
           </p>
           <APITester />
         </CardContent>
